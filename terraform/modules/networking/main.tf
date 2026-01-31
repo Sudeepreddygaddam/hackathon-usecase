4436c8b1e0b713448d600d0d7bc7c35f1c7a0987
Gaddam Sudeep Kumar Reddy <156163418+Sudeepreddygaddam@users.noreply.github.com>
2026-01-31 21:35:32 +0530

    changes

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -1,3 +1,21 @@
+# 1. Provider Configuration
+provider "google" {
+  project = "project-f561a2e0-7d0d-4ebe-948"
+  region  = var.region
+}
+
+# 2. Variable Definitions (Crucial for the resources below)
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+# 3. Networking Resources
 resource "google_compute_network" "main" {
   name                    = "vpc-${var.env}"
   auto_create_subnetworks = false
@@ -23,4 +41,13 @@ resource "google_compute_router_nat" "nat" {
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+# 4. Outputs (To use in your GKE module)
+output "vpc_id" {
+  value = google_compute_network.main.id
+}
+
+output "subnet_id" {
+  value = google_compute_subnetwork.private.id
 }
